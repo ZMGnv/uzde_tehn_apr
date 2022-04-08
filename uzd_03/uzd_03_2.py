b0be1547e312a711ecd8d9bbb81fbef7c5ff1065
Dtoce <cape@inbox.lv>
2022-04-08 20:03:43 +0300

    8.aprīļa izmaiņas

--- uzd_03/uzd_03_2.py
+++ uzd_03/uzd_03_2.py
@@ -1,2 +1,11 @@
 # Atrast cik skaitļi norādītajā intervālā dalās ar lietotāja norādīto dalītāju.
 
+sakt=int(input("Sākuma skaitlis:"))
+beigt=int(input("Beigu skaitlis:"))
+dal=int(input("Dalītājs:"))
+
+cik=0
+for i in range(sakt,beigt+1):
+    if i%dal==0:
+        cik+=1 # cik=cik+1
+print(f"Robežās starp {sakt} un {beigt} -  {cik} skaitļi dalās ar {dal}")
