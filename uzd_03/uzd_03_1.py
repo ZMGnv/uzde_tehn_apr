026ff90ace289022ba020855e133ba3215d18e8d
Dtoce <cape@inbox.lv>
2022-04-08 20:04:04 +0300

    viss

--- uzd_03/uzd_03_1.py
+++ uzd_03/uzd_03_1.py
@@ -1,2 +1,14 @@
-# Apgriezt pozitīva skaitļa ciparu secību. Piemērs: 12345 => 54321
+# Apgriezt pozitīva skaitļa ciparu secību.
+#  Piemērs: 12345 => 54321
 
+skaitlis=input("Ievadi pozitīvu skaitli:")
+
+try:
+    sk=int(skaitlis)
+    if sk>0:
+        for i in range(len(skaitlis)-1,-1, -1):
+            print(skaitlis[i], end="")
+    else:
+        print("Nav ievad;its pozitīvs skaitlis!")
+except ValueError:
+    print("Nav ievadīts skaitlis")
